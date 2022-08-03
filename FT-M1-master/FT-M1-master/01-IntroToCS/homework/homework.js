13431a580086fb1274ed691f825cc25f30266523
unknown <diego.gomezs856@gmail.com>
2022-08-03 20:13:01 -0300

    Se agrega homexork

--- FT-M1-master/FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,18 +2,40 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  let sum = 0;
+
+  const i = 10;
+  var resto = 0;
+  var cont = 1;
+  var resultado = 0;
+  while (num > 0){
+   resto = num % i;
+   num = Math.floor(num/i);
+   resultado += resto*cont;
+   cont*=2; 
+  }
+  return resultado;
+  /*let sum = 0;
 
   for (let i = 0; i < num.length; i++) {
      sum += +num[i] * 2 ** (num.length - 1 - i);
   }
-  return sum;
+  return sum;*/
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-  return num.toString(2)
+  const i = 2;
+  var resto = 0;
+  var cont = 1;
+  var resultado = 0;
+  while (num > 0){
+   resto = num % i;
+   num = Math.trunc(num/i);
+   resultado += resto*cont;
+   cont*=10; 
+   }
+  return resultado.toString();
 }
 
 
