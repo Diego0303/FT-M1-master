c92d72d2107b51a860aa06dd4405b913e6b3ca89
unknown <diego.gomezs856@gmail.com>
2022-09-02 16:52:59 -0300

    homework

--- FT-M1-master/FT-M1-master/08-Algoritmos-II/homework/homework.js
+++ FT-M1-master/FT-M1-master/08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,20 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) return array;
 
+  let pivot = array [0];
+  let left = [];
+  let right = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if ( array[i] > pivot){
+      right.push(array[i]);
+    } else {
+      left.push (array[i]);
+    }
+  }
+  return quickSort(left).concat(pivot).concat(quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,7 +27,40 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+//caso base
+  if (array.length === 1) return array;
 
+  let division = split(array);
+  let left = division[0];
+  let right = division[1];
+
+  return paste(mergeSort(left), mergeSort(right));
+}
+
+//funcion que divide el arreglo
+function split(array){
+  let middle = Math.floor(array.length / 2);
+  let left = array.slice (0, middle);
+  let right = array.slice (middle);
+
+  return [left, right];
+}
+
+function paste(left, right) {
+  let array = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length){
+    if (left[leftIndex] < right[rightIndex]){
+      array.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      array.push(right[rightIndex])
+      rightIndex++
+    }
+  }
+  return array.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 // No modificar nada debajo de esta línea
